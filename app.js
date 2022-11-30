5b0497cab70f1470e6bba84c03337a439ba2fbc0
nacjji <najiwon1106@gmail.com>
2022-11-30 22:28:56 +0900

    drop useless codes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const Posts = require("./route/posts.js");
-const Comments = require("./route/comments.js");
+
 const connect = require("./schemas");
 const indexRouter = require("./route/index");
 connect();
